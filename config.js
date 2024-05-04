8f5ac2750f633441abd251d87dafaa26e1b23aa8
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-05-04 19:00:36 -0300

    segundo commit

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // firebase config key setup
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-//Web app's firebase configuration
+//firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAKZauoVxK0mS311h40VXgIWJ5JxSpMyTQ",
   authDomain: "neurosphere-132f0.firebaseapp.com",
@@ -15,8 +14,8 @@ const firebaseConfig = {
   measurementId: "G-RHFBV6FTME"
 };
 
-  if(!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+if(!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-  export {firebase}
+export {firebase}
