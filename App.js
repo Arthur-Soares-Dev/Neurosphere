05d5e758d5763bdd72196434c4707c407b8db2d9
GregoryRFGMS <124697446+GregoryRFGMS@users.noreply.github.com>
2024-05-04 22:19:33 -0300

    Update 0.2
    Remoção da Header (Comentarios Adicionados!)
    
    Mudançã do Design Base da Tela de Login

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React, {useState, useEffect} from "react";
-import {firebase} from "./config";
+import React, { useState, useEffect } from "react";
+import { firebase } from "./config";
 
 import Login from "./src/Login";
 import Cadastro from "./src/Cadastro";
 import Dashboard from "./src/Dashboard";
-import Header from "./components/Header";
 
 const Stack = createStackNavigator();
 
@@ -26,61 +25,32 @@ function App() {
     }, [])
     if (initializing) return null;
 
-    if (!user) {
-        return (
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Login" 
-                    component={Login}
-                    options={{
-                        headerTittle: () => <Header name="NeuroSphere"/>,
-                        headerStyle: {
-                            height: 150,
-                            backgroundColor: '#FD7FAC',
-                            shadowColor: '#000',
-                            elevation: 25
-                        }
-                    }}
-                    />
-                <Stack.Screen
-                    name="Cadastro" 
-                    component={Cadastro}
-                    options={{
-                        headerTittle: () => <Header name="NeuroSphere"/>,
-                        headerStyle: {
-                            height: 150,
-                            backgroundColor: '#FD7FAC',
-                            shadowColor: '#000',
-                            elevation: 25
-                        }
-                    }}
-                    />
-            </Stack.Navigator>
-        );
-    }
     return (
-        <Stack.Navigator>
-            <Stack.Screen
-                    name="Tela inicial" 
-                    component={Dashboard}
-                    options={{
-                        headerTittle: () => <Header name="Dashboard"/>,
-                        headerStyle: {
-                            height: 150,
-                            backgroundColor: '#FD7FAC',
-                            shadowColor: '#000',
-                            elevation: 25
-                        }
-                    }}
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{ headerShown: false }} // Tem configurar headerShown como false para cada tela no Stack.Navigator (Isso faz com que suma a Header Feia)
+
+            >
+                {!user ? (
+                    <>
+                        <Stack.Screen
+                            name="Login"
+                            component={Login}
+                        />
+                        <Stack.Screen
+                            name="Cadastro"
+                            component={Cadastro}
+                        />
+                    </>
+                ) : (
+                    <Stack.Screen
+                        name="Dashboard"
+                        component={Dashboard}
                     />
-        </Stack.Navigator>
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
-export default () => {
-    return (
-        <NavigationContainer>
-            <App/>
-        </NavigationContainer>
-    )
-} // Meu parabens caro leitor!
+export default App;
