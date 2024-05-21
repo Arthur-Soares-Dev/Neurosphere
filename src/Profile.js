b8ead2a1a88cde3bc0c0960dde8da929691ddcc9
GregoryRFGMS <124697446+GregoryRFGMS@users.noreply.github.com>
2024-05-21 15:41:54 -0700

    Update 1.2
    Adicão Página de perfil de usuario (Base)
    
    - Funcionalidade de Transicionar entre Dashboard e Perfil de Usuario

---
+++ src/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Profile = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Perfil do Usuário</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
