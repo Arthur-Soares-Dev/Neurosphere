5bd2407b132f19addaa2e0f35da49bfe9bba3d2a
Kai-Ofc <karenluisa980@gmail.com>
2024-06-19 18:30:09 -0300

    Update 1.7 - Component Dashboard 3
    Mudança da Barra de Status e Atalho para o perfil

---
+++ src/components/Dashboard/DashboardHeader/index.js
@@ -0,0 +1,32 @@
+import { View, StyleSheet, StatusBar } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+        <StatusBar
+        barStyle="light-content"
+        backgroundColor="#FD7FAC"
+        translucent={false}/>
+        <View style={styles.profile}></View>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 30,
+        backgroundColor: '#FD7FAC',
+        paddingLeft: 10,
+        marginBottom: 30,
+    },
+
+    profile: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        backgroundColor: '#D9D9D9',
+    },
+});
