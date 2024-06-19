feffdab43949af9f4dcef7a0474eda74e5b873f9
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-06-19 19:39:11 -0300

    Criação da tela FrasesAudio

---
+++ src/FrasesAudio.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FrasesAudio = () => {
+  return (
+    <View>
+      <Text>FrasesAudio</Text>
+    </View>
+  )
+}
+
+export default FrasesAudio
