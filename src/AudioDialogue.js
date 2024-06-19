64e7fdc992d89641ed4ac6aede4d6a75df1681a8
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-06-19 20:10:45 -0300

    nome da pagina de frases alterado

---
+++ src/AudioDialogue.js
@@ -0,0 +1,42 @@
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { firebase } from '../config';
+
+const AudioDialogue = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>
+        Frases
+      </Text>
+
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    backgroundColor: '#f0f0f0',
+},
+button: {
+    marginTop: 20,
+    height: 70,
+    width: '100%',
+    backgroundColor: '#026efd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+},
+buttonText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#fff',
+}
+});
+export default AudioDialogue
