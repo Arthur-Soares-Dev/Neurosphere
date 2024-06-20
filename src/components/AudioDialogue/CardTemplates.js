9c1e2287ee7587557990549fcf9c70304faac1e6
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-06-19 21:24:09 -0300

    AudioDialogue update
    Criei um componente para os card.
    Criei um arquivo de CardTemplates para a os cards padrão

---
+++ src/components/AudioDialogue/CardTemplates.js
@@ -0,0 +1,14 @@
+const d = '../AudioDialogue/CardImages/'
+
+export const cardTemplates = [
+    {
+        title: 'SIM',
+        audio: 'Para onde vamos?',
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: 'NÃO',
+        audio: 'NÃO',
+        uri: require('./CardImages/x.png')
+    }
+]
