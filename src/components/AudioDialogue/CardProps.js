d4b9d1a8039ecdda2efa1b242e6330165f01ab1e
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-06-22 13:41:10 -0300

    Cardprops update

--- src/components/AudioDialogue/CardProps.js
+++ src/components/AudioDialogue/CardProps.js
@@ -12,32 +12,32 @@ export const cardProps = [
     {
         title: "QUERO",
         audio: "Eu quéro.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/quero.png')
     },
     {
         title: "GOSTEI",
         audio: "Eu gostei.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/gostei.png')
     },
     {
         title: "AJUDA",
         audio: "Eu preciso de ajuda.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/ajuda.png')
     },
     {
         title: "COMER",
         audio: "Eu quero comer.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/comer.png')
     },
     {
         title: "DESCANSAR",
         audio: "Eu quero descansar.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/descansar.png')
     },
     {
         title: "BRINCAR",
         audio: "Eu quero brincar.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/brincar.png')
     },
     {
         title: "TRISTE",
@@ -47,7 +47,7 @@ export const cardProps = [
     {
         title: "FELIZ",
         audio: "Eu estou feliz.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/feliz.png')
     },
     {
         title: "BANHEIRO",
@@ -62,21 +62,16 @@ export const cardProps = [
     {
         title: "CASA",
         audio: "Eu quero ir para casa.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/casa.png')
     },
     {
         title: "DORMIR",
         audio: "Eu quero dormir.",
-        uri: require('./CardImages/check.png')
-    },
-    {
-        title: "FOME",
-        audio: "Eu estou com fome.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/dormir.png')
     },
     {
         title: "SEDE",
         audio: "Eu estou com sede.",
-        uri: require('./CardImages/check.png')
+        uri: require('./CardImages/sede.png')
     }
 ]
