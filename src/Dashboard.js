eb3583eaccdc989b1eb3c234aab58d6d77a967d0
Kai-Ofc <karenluisa980@gmail.com>
2024-06-22 18:57:08 -0300

    Dashboard Update
    Design do Dashboard.
    Tarefas do Dashboard Incompletas

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,8 @@
-import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View, ScrollView } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { firebase } from '../config';
 import TaskList from './components/Dashboard/DashboardTasks/index'
+import Card from './components/Dashboard/DashboardCard/index'
 
 const Dashboard = ({ navigation }) => {
   const [usuario, setUsuario] = useState(null);
@@ -36,18 +37,22 @@ const Dashboard = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
+
+        <Text style={styles.headerText}>
+          Olá, {usuario?.name}!
+        </Text>
+
         <TouchableOpacity
           style={styles.profileButton}
           onPress={() => navigation.navigate('Profile')}
         >
           <View style={styles.profileIcon} />
         </TouchableOpacity>
-        <Text style={styles.headerText}>
-          Olá, {usuario?.name}!
-        </Text>
       </View>
 
-      <TouchableOpacity
+      <Card/>
+
+      {/*<TouchableOpacity
         onPress={() => changePassword()}
         style={styles.button}
       >
@@ -98,11 +103,90 @@ const Dashboard = ({ navigation }) => {
         <Text style={styles.buttonText}>
           Frases
         </Text>
-      </TouchableOpacity>
+      </TouchableOpacity>*/}
 
-      <TaskList
 
-      />
+      <SafeAreaView style={styles.containerAtalhos}>
+
+        <View style={styles.menuContainer}>
+            <Text style={{fontSize: 20}}>Atalhos Diários</Text>
+        </View>
+
+        <ScrollView
+        horizontal={true}  
+        contentContainerStyle={styles.scrollContainer}
+        showsHorizontalScrollIndicator={false}
+        style={{height: 150}}
+        >
+            <TouchableOpacity style={styles.cardPink}> 
+                <View style={styles.circle}>
+                    <Text style={styles.textCircle}>
+                        +
+                    </Text>
+                </View>
+                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
+                    Adicionar Tarefa
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.cardBlue}
+            onPress={() => navigation.navigate('TelaDasCrianca')}
+            > 
+              <View style={styles.square}> 
+              </View>
+              <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
+                Ver {"\n"} Tarefas
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.card}
+            onPress={() => navigation.navigate('AudioDialogue')}
+            > 
+              <View style={styles.circle}>
+                <Text style={{ color: "#FD7FAC", fontSize: 40, marginTop: -6, }}>
+                    
+                </Text>
+              </View>
+              <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
+                Painel de Frases
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.cardPink}
+            onPress={() => changePassword()}
+            > 
+                <View style={styles.circle}>
+                  <Text style={{ color: "#FD7FAC", fontSize: 40, marginTop: -6, }}></Text>
+                </View>
+                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
+                  Trocar a {"\n"} Senha
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.cardBlue}
+            onPress={() => {
+              firebase.auth().signOut()
+                .then(() => {
+                  navigation.navigate('Login');
+                })
+                .catch((error) => {
+                  console.error("Erro ao fazer logout:", error);
+                });
+            }}
+            > 
+                <View style={styles.circle}>
+                  <Text style={{position: 'absolute', color: "#7FACD6", fontSize: 40, textAlign: 'center', top: -9, left: 15}}>
+                    {">"}
+                  </Text>
+                </View>
+                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
+                  {"\n"} Sair
+                </Text>
+            </TouchableOpacity>
+        </ScrollView>
+        </SafeAreaView>
+
+      <TaskList/>
 
 
     </SafeAreaView>
@@ -116,9 +200,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    paddingTop: 20,
-    paddingHorizontal: 20,
-    backgroundColor: '#f0f0f0',
+    paddingTop: 40,
+    backgroundColor: '#fff',
   },
   header: {
     width: '100%',
@@ -126,6 +209,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 20,
+    paddingLeft: 10,
   },
   headerText: {
     fontSize: 20,
@@ -135,10 +219,10 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   profileIcon: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    backgroundColor: '#fff', // bola branca
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#f0f0f0', // bola branca
   },
   button: {
     marginTop: 20,
@@ -153,5 +237,89 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     color: '#fff',
-  }
+  },
+
+  containerAtalhos: {
+    height: 200
+  },
+
+  menuContainer: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    marginBottom: 10,
+  },
+
+  scrollContainer: {
+    height: 150,
+    flexDirection: "row",
+    paddingLeft: 10,
+    paddingRight: 10,
+    justifyContent:'space-between',
+  },
+
+  card: {
+    width: 125,
+    height: 150,
+    borderRadius: 10,
+    backgroundColor: '#353535',
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  cardPink: {
+    width: 125,
+    height: 150,
+    borderRadius: 10,
+    backgroundColor: '#FD7FAC',
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  cardBlue: {
+    width: 125,
+    height: 150,
+    borderRadius: 10,
+    backgroundColor: '#7FACD6',
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  circle:{
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  textCircle: { 
+    position: 'absolute',
+    color: "#FD7FAC", 
+    fontSize: 40, 
+    textAlign: 'center' 
+  },
+
+  square:{
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
 });
