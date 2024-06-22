0bf3fa1c72d6156256333493e9a2dc90dcbb5c37
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-06-21 23:15:37 -0300

    Update cardProps
    adição de frases no cardProps e pequena alteração do card

--- src/components/AudioDialogue/Card.js
+++ src/components/AudioDialogue/Card.js
@@ -1,45 +1,39 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native'
 import React, { useState } from 'react';
 import * as Speech from 'expo-speech';
-import { cardTemplates } from './CardTemplates';
+import { cardProps } from './CardProps';
 
 const Card = () => {
     const [texto, setTexto] = useState('inicio');
     const [title, setTitle] = useState(null);
     const [audio, setAudio] = useState(null);
     const [image, setImage] = useState(null);
-
     const speak = (audio) => {
         const thingToSay = audio;
         options = {
             language: 'pt-BR',
-            rate: 0.85
+            rate: 0.9
             //Documentação com todas as opções, como volume, velocidade, e essas coisas
             //https://docs.expo.dev/versions/latest/sdk/speech/#speechoptions
         }
         Speech.speak(thingToSay, options);
     }
-    const cardList = cardTemplates.map((template, index) => {
+    const cardList = cardProps.map((template, index) => {
         return (
-            <SafeAreaView key={index}>
-                <TouchableOpacity style={styles.button} onPress={() => speak(template.audio)}>
+                <TouchableOpacity key={index} style={styles.button} onPress={() => speak(template.audio)}>
                             <Text style={styles.buttonText}>{template.title}</Text>
                             <Image
                                 style={styles.tinyLogo}
                                 source={template.uri}
                             />
                     </TouchableOpacity>
-            </SafeAreaView>
           )
     })
-
     return (
-        <SafeAreaView style={styles.container}>
+            <ScrollView styles={styles.container}>
             {cardList}
-        </SafeAreaView>
+            </ScrollView>
     )
-
-  
 }
 
 export default Card
---
+++ src/components/AudioDialogue/CardProps.js
@@ -0,0 +1,82 @@
+export const cardProps = [
+    {
+        title: "SIM",
+        audio: "Sim.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "NÃO",
+        audio: "Não.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "QUERO",
+        audio: "Eu quéro.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "GOSTEI",
+        audio: "Eu gostei.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "AJUDA",
+        audio: "Eu preciso de ajuda.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "COMER",
+        audio: "Eu quero comer.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "DESCANSAR",
+        audio: "Eu quero descansar.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "BRINCAR",
+        audio: "Eu quero brincar.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "TRISTE",
+        audio: "Eu estou triste.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "FELIZ",
+        audio: "Eu estou feliz.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "BANHEIRO",
+        audio: "Eu preciso ir ao banheiro.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "DOR",
+        audio: "Eu estou com dor.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "CASA",
+        audio: "Eu quero ir para casa.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "DORMIR",
+        audio: "Eu quero dormir.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "FOME",
+        audio: "Eu estou com fome.",
+        uri: require('./CardImages/check.png')
+    },
+    {
+        title: "SEDE",
+        audio: "Eu estou com sede.",
+        uri: require('./CardImages/check.png')
+    }
+]
