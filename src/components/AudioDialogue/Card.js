a539a779fbdc4a3137e81d4b921a564a746eba9c
K <168924521+Kai-Ofc@users.noreply.github.com>
2024-07-25 17:33:43 -0300

    AudioDialog Update
    Mudança no styleSheet

--- src/components/AudioDialogue/Card.js
+++ src/components/AudioDialogue/Card.js
@@ -1,55 +1,136 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native'
-import React, { useState } from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native'
+// import React, { useState } from 'react';
+// import * as Speech from 'expo-speech';
+// import { cardProps } from './CardProps';
+
+// const Card = () => {
+//     const [texto, setTexto] = useState('inicio');
+//     const [title, setTitle] = useState(null);
+//     const [audio, setAudio] = useState(null);
+//     const [image, setImage] = useState(null);
+//     const speak = (audio) => {
+//         const thingToSay = audio;
+//         options = {
+//             language: 'pt-BR',
+//             rate: 0.9
+//             //Documentação com todas as opções, como volume, velocidade, e essas coisas
+//             //https://docs.expo.dev/versions/latest/sdk/speech/#speechoptions
+//         }
+//         Speech.speak(thingToSay, options);
+//     }
+//     const cardList = cardProps.map((template, index) => {
+//         return (
+//             <TouchableOpacity key={index} style={styles.button} onPress={() => speak(template.audio)}>
+//                 <Text style={styles.buttonText}>{template.title}</Text>
+//                 <Image
+//                     style={styles.tinyLogo}
+//                     source={template.uri}
+//                 />
+//             </TouchableOpacity>
+//         )
+//     })
+//     return (
+//         <ScrollView styles={styles.container}>
+//         {cardList}
+//         </ScrollView>
+//     )
+// }
+
+// export default Card
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         flexDirection: 'row',
+//         paddingTop: 20,
+//         paddingHorizontal: 20,
+//         backgroundColor: '#f0f0f0',
+//     },
+//     button: {
+//         marginTop: 20,
+//         height: 125,
+//         width: 150,
+//         backgroundColor: '#026efd',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         borderRadius: 15,
+//     },
+//     buttonText: {
+//         fontSize: 22,
+//         fontWeight: 'bold',
+//         color: '#fff',
+//     },
+//     tinyLogo: {
+//       width: 50,
+//       height: 50,
+//     }
+//     });
+
+import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, FlatList } from 'react-native';
+import React from 'react';
 import * as Speech from 'expo-speech';
 import { cardProps } from './CardProps';
 
 const Card = () => {
-    const [texto, setTexto] = useState('inicio');
-    const [title, setTitle] = useState(null);
-    const [audio, setAudio] = useState(null);
-    const [image, setImage] = useState(null);
+    // Defina as cores da borda em ordem
+    const borderColors = ['#FD7FAC', '#7FACD6', '#353535'];
+
     const speak = (audio) => {
         const thingToSay = audio;
-        options = {
+        const options = {
             language: 'pt-BR',
-            rate: 0.9
-            //Documentação com todas as opções, como volume, velocidade, e essas coisas
-            //https://docs.expo.dev/versions/latest/sdk/speech/#speechoptions
-        }
+            rate: 0.9,
+        };
         Speech.speak(thingToSay, options);
-    }
-    const cardList = cardProps.map((template, index) => {
-        return (
-                <TouchableOpacity key={index} style={styles.button} onPress={() => speak(template.audio)}>
-                            <Text style={styles.buttonText}>{template.title}</Text>
-                            <Image
-                                style={styles.tinyLogo}
-                                source={template.uri}
-                            />
-                    </TouchableOpacity>
-          )
-    })
-    return (
-            <ScrollView styles={styles.container}>
-            {cardList}
-            </ScrollView>
-    )
-}
+    };
 
-export default Card
+    // Define o layout dos itens para ter 2 colunas
+    const renderItem = ({ item, index }) => {
+        // Determine a cor da borda com base no índice do item
+        const borderColor = borderColors[index % borderColors.length];
+
+        return (
+            <TouchableOpacity
+                style={[styles.button, { borderColor: borderColor }]}
+                onPress={() => speak(item.audio)}
+            >
+                <Text style={styles.buttonText}>{item.title}</Text>
+                <Image style={styles.tinyLogo} source={item.uri} />
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={cardProps}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.title}
+                numColumns={2}
+                columnWrapperStyle={styles.row}
+                showsVerticalScrollIndicator={false}
+            />
+        </SafeAreaView>
+    );
+};
+
+export default Card;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         paddingTop: 20,
-        paddingHorizontal: 20,
         backgroundColor: '#f0f0f0',
     },
+    row: {
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
     button: {
-        marginTop: 20,
-        height: 70,
-        width: '100%',
-        backgroundColor: '#026efd',
+        height: 150,
+        width: '48%',
+        backgroundColor: '#f0f0f0', 
+        borderColor: '#026efd', 
+        borderWidth: 2, 
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 15,
@@ -57,10 +138,14 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 22,
         fontWeight: 'bold',
-        color: '#fff',
+        color: '#353535', 
+        marginBottom: 20,
     },
     tinyLogo: {
-      width: 50,
-      height: 50,
-    }
-    });
+        width: 50,
+        height: 50,
+    },
+});
+
+
+
--- src/components/Dashboard/DashboardTasks/index.js
+++ src/components/Dashboard/DashboardTasks/index.js
@@ -130,67 +130,6 @@ export default function Tasks() {
             return taskDate.getDay();
     });
 
-
-  /*return (
-    <View style={{width: "95%", marginTop: 10}}>
-        <View style={styles.menuContainer}>
-            <Text style={{fontSize: 20}}>Tarefas Diárias</Text>
-        </View>
-        <View style={styles.card}>
-            <ScrollView
-            horizontal = {true}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.scrollContainer}
-            >
-                <TouchableOpacity style={styles.cardPink}> 
-                <View style={styles.circle}>
-                    <Text style={styles.textCircle}>
-                        +
-                    </Text>
-                </View>
-                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
-                    Adicionar Tarefa
-                </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.cardPink}> 
-                <View style={styles.circle}>
-                    <Text style={styles.textCircle}>
-                        +
-                    </Text>
-                </View>
-                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
-                    Adicionar Tarefa
-                </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.cardPink}> 
-                <View style={styles.circle}>
-                    <Text style={styles.textCircle}>
-                        +
-                    </Text>
-                </View>
-                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
-                    Adicionar Tarefa
-                </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.cardPink}> 
-                <View style={styles.circle}>
-                    <Text style={styles.textCircle}>
-                        +
-                    </Text>
-                </View>
-                <Text style={{ color: "white", fontSize: 14, textAlign: 'center' }}>
-                    Adicionar Tarefa
-                </Text>
-            </TouchableOpacity>
-            </ScrollView>
-            
-        </View>
-    </View>
-  )*/
-
     return (
         <SafeAreaView style={{width: "95%", minHeight: 180, marginTop: 10}}>
             <View style={styles.menuContainer}>
@@ -212,46 +151,6 @@ export default function Tasks() {
         
     );
 }
-/*
-const styles = StyleSheet.create({
-    menuContainer: {
-        alignItems: 'flex-end',
-        flexDirection: 'row',
-        justifyContent:'space-between',
-        marginBottom: 10,
-    },
-
-    card: {
-        backgroundColor: '#fff',
-        borderWidth: 1,
-        borderColor: '#E3E3E3',
-        marginBottom: 17,
-        borderRadius: 10,
-        paddingTop: 10,
-        paddingBottom: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    scrollContainer:{
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-
-    //Tá quebrado. Os cards não ocupam o tamanho inteiro do scroll. Concertar
-    cardPink: {
-        height: 100,
-        borderRadius: 10,
-        backgroundColor: '#FD7FAC',
-        justifyContent: "center",
-        alignItems: "center",
-        marginRight: 10,
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-});*/
-
-
 
 const styles = StyleSheet.create({
 
