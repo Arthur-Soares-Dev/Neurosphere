2bd580b62d3ab510c14b1a84d859dde1088c813b
K <168924521+Kai-Ofc@users.noreply.github.com>
2024-10-28 20:19:40 -0300

    Update baseButton.jsx

--- client/src/components/BasesComponents/baseButton.jsx
+++ client/src/components/BasesComponents/baseButton.jsx
@@ -2,13 +2,13 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import globalStyles from '../../Styles/GlobalStyle';
 
-const SocialLoginButtons = ({...props}) => {
+const SocialLoginButtons = (tittle, {...props}) => {
   return (
     <TouchableOpacity
     onPress={() => handleLogin(email, password)}
     style={globalStyles.button}
     >
-        <Text style={globalStyles.buttonText}>Logar</Text>
+        <Text style={globalStyles.buttonText}> {tittle} </Text>
     </TouchableOpacity>
   );
 };
