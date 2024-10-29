e5215ebe615fce33f981c63271f501620a191b46
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-10-28 21:19:54 -0300

    base input

--- client/src/components/BasesComponents/baseInput.jsx
+++ client/src/components/BasesComponents/baseInput.jsx
@@ -1,23 +1,55 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
-import globalStyles from '../../Styles/GlobalStyle';
+import {StyleSheet, TextInput, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import globalStyles, {colors} from '../../Styles/GlobalStyle';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Utils from "../../utils/Utils";
 
-const SocialLoginButtons = ({...props}) => {
-  return (
-    <View style={globalStyles.input}>
-    <TextInput
-      style={globalStyles.inputText}
-      onChangeText={(email) => setEmail(email)}
-      autoCapitalize="none"
-      autoCorrect={false}
-      value={email}
-    />
-  </View>
-  );
+const StyledInput = ({...props}) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const {
+        filled,
+        variant,
+        value,
+    } = props;
+    let inputStyles = [globalStyles.inputText];
+    let viewStyles = [globalStyles.input];
+    if (filled) {
+        inputStyles.push(globalStyles.filledInputText)
+        viewStyles.push(globalStyles.filledInput)
+    }
+
+
+    return (
+        <View style={viewStyles}>
+            <TextInput
+                style={inputStyles}
+                secureTextEntry={!showPassword}
+                {...props}
+            />
+
+            {(value && variant === 'email') && (
+                <Ionicons
+                    name={Utils.validateEmail(value) ? "checkmark-circle" : "close-circle"}
+                    size={24}
+                    color={Utils.validateEmail(value) ? colors.BLUE : colors.BLUE}
+                    style={styles.inputIcon}
+                />
+            )}
+
+            {(variant === 'password') && (
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                    <Ionicons
+                        name={showPassword ? "eye" : "eye-off"}
+                        size={24}
+                        color={colors.WHITE}
+                        style={styles.inputIcon}
+                    />
+                </TouchableOpacity>
+            )}
+        </View>
+    );
 };
 
-export default SocialLoginButtons;
+export default StyledInput;
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
