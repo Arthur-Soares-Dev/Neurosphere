a9264d08774db6a1b74ede38895f0deda9135ff0
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-11-02 12:08:45 -0300

    mudança dependencias

--- client/src/service/api.jsx
+++ client/src/service/api.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import BASE_URL from './ipLocal'
-
-let url = ''
-if (BASE_URL) {
-  url = BASE_URL;
-} else {
-  url = '10.0.2.2';
-}
+// import BASE_URL from './ipLocal'
+//
+// let url = ''
+// if (BASE_URL) {
+//   url = BASE_URL;
+// } else {
+//   url = '10.0.2.2';
+// }
 
 const api = axios.create({
   // baseURL: `http://${url}:5000/`,
