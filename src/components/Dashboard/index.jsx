66564cf910e5905585fefee400fc6974f77ccf4a
K <168924521+Kai-Ofc@users.noreply.github.com>
2024-11-21 22:03:08 -0300

    Gif Fix

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,17 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
-import globalStyles, {colors, sizeFonts} from '../../Styles/GlobalStyle';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import globalStyles, { colors, sizeFonts } from '../../Styles/GlobalStyle';
 import DashGif from '../../../assets/DashGif.gif';
 import DashStatic from '../../../assets/DashStatic.png';
 
 const Card = () => {
   const [isPlaying, setIsPlaying] = useState(true); // Começa reproduzindo o GIF automaticamente
+  const [isDisabled, setIsDisabled] = useState(true); // Desabilita o botão enquanto o GIF está ativo
 
   const gifDuration = 4350; // Duração do GIF em milissegundos
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsPlaying(false); // Troca para a imagem estática após o GIF terminar
+      setIsDisabled(false); // Habilita o botão
     }, gifDuration);
 
     return () => clearTimeout(timer); // Limpa o timer ao desmontar o componente
@@ -19,24 +21,31 @@ const Card = () => {
 
   const restartGif = () => {
     setIsPlaying(true); // Reinicia o GIF ao clicar
+    setIsDisabled(true); // Desabilita o botão enquanto o GIF está ativo
     setTimeout(() => {
       setIsPlaying(false); // Troca para a imagem estática após o GIF terminar
+      setIsDisabled(false); // Habilita o botão
     }, gifDuration);
   };
 
   return (
-    <View style={{width: '100%'}}>
+    <View style={{ width: '100%' }}>
       <View style={styles.menuContainer}>
         <Text
           style={{
             fontSize: sizeFonts.MEDIUM,
             color: colors.YELLOW,
             fontFamily: 'MinhaFonte',
-          }}>
+          }}
+        >
           INÍCIO
         </Text>
       </View>
-      <TouchableOpacity style={styles.gifButton} onPress={restartGif}>
+      <TouchableOpacity
+        style={[styles.gifButton, isDisabled && styles.disabledButton]}
+        onPress={restartGif}
+        disabled={isDisabled} // Desabilita o botão enquanto o GIF está ativo
+      >
         {isPlaying ? (
           <Image source={DashGif} style={styles.gif} /> // Exibe o GIF enquanto reproduz
         ) : (
@@ -70,7 +79,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 17,
-    // paddingTop: 25,
+  },
+
+  disabledButton: {
+    opacity: 1, // Adiciona um efeito visual para indicar que o botão está desabilitado
   },
 
   gif: {
